44bafe0fff50afbdb5f43353e7a19e917418207a
Rajini Preetha John <rajinipreethaj@hotmail.com>
2025-05-29 11:28:43 +0800

    'apple-touch-icon added'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
 
